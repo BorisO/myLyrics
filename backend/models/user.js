7d747ba371d402ffefc1adc995ab689a009ec82a
Boris Onufriyev <borisonufriyev@yahoo.com>
2019-02-18 14:10:14 -0500

    user model, auth middleware, other basic file setups. installed bcrypt, jwt, lodash

---
+++ backend/models/user.js
@@ -0,0 +1,72 @@
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+
+const User = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 50
+    },
+    email: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 255,
+      unique: true
+    },
+    username: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 30,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 5,
+      maxlength: 1024
+    },
+    isAdmin: Boolean
+  })
+);
+
+userSchema.methods.generateAuthToken = function() {
+  const token = jwt.sign(
+    { _id: this._id, isAdmin: this.isAdmin },
+    config.get("jwtPrivateKey")
+  );
+  return token;
+};
+
+function validateUser(user) {
+  const schema = {
+    name: Joi.string()
+      .min(5)
+      .max(50)
+      .required(),
+    email: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+      .email(),
+    username: Joi.string()
+      .min(5)
+      .max(30)
+      .required(),
+    password: Joi.string()
+      .min(5)
+      .max(255)
+      .required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,3 +1,7 @@
+const { User, validate } = require("../models/user");
+const config = require("config");
+const _ = require("lodash");
+const bcrypt = require("bcrypt");
 const express = require("express");
 const router = express.Router();
 
